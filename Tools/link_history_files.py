6fc0a5b61200dc86e329a591328a0ca1d1847d61
Alice Bertini <aliceb@ucar.edu>
2017-09-13 20:35:42 -0600

    bug fixes for issue #91

--- Tools/link_history_files.py
+++ Tools/link_history_files.py
@@ -32,12 +32,12 @@ import os
 for year in ['1960','1961','1962','1963','1964','1965','1966','1967','1968','1969']:
     for month in ['01','02','03','04','05','06','07','08','09','10','11','12']:
         
-        srcdir = '/glade/p/cesm0005/archive/b.e20.BHIST.f09_g17.20thC.195_01/lnd/hist'
-        linkdir = '/glade/scratch/aliceb/b.e20.BHIST.f09_g17.20thC.195_01/lnd/hist'
+        srcdir = '/glade/p/cesm0005/archive/b.e20.BHIST.f09_g17.20thC.192_01/ocn/hist'
+        linkdir = '/glade/scratch/aliceb/b.e20.BHIST.f09_g17.20thC.192_01/ocn/hist'
 
         if not os.path.exists(linkdir):
             os.makedirs(linkdir)
 
-        casename = 'b.e20.BHIST.f09_g17.20thC.195_01.clm2.h0'
+        casename = 'b.e20.BHIST.f09_g17.20thC.192_01.pop.h'
         filename = '{0}.{1}-{2}.nc'.format(casename,year,month)
         os.symlink(os.path.join(srcdir, filename), os.path.join(linkdir, filename))
