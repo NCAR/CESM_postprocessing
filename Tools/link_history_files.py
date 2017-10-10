187af9b302777536238ca7e4c1e1106a982cfbfb
Alice Bertini <aliceb@ucar.edu>
2017-10-10 12:49:24 -0600

    update ilamb and iomb XML settings in Machines for cheyenne

--- Tools/link_history_files.py
+++ Tools/link_history_files.py
@@ -29,15 +29,28 @@ import os
 ##        filename = '{0}.{1}-{2}-01.nc'.format(casename,year,month)
 ##        os.symlink(os.path.join(srcdir, filename), os.path.join(linkdir, filename))
 
-for year in ['1960','1961','1962','1963','1964','1965','1966','1967','1968','1969']:
+for year in ['0101','0102','0103','0104','0105','0106','0107','0108','0109','0110','0111','0112','0113','0114','0115','0116','0117','0118','0119','0120']:
     for month in ['01','02','03','04','05','06','07','08','09','10','11','12']:
         
-        srcdir = '/glade/p/cesm0005/archive/b.e20.BHIST.f09_g17.20thC.192_01/ocn/hist'
-        linkdir = '/glade/scratch/aliceb/b.e20.BHIST.f09_g17.20thC.192_01/ocn/hist'
+        srcdir = '/glade/p/cesm0005/archive/b.e20.B1850.f09_g17.pi_control.all.197/ocn/hist'
+        linkdir = '/glade/scratch/aliceb/b.e20.B1850.f09_g17.pi_control.all.197/ocn/hist'
 
         if not os.path.exists(linkdir):
             os.makedirs(linkdir)
 
-        casename = 'b.e20.BHIST.f09_g17.20thC.192_01.pop.h'
+        casename = 'b.e20.B1850.f09_g17.pi_control.all.197.pop.h'
+        filename = '{0}.{1}-{2}.nc'.format(casename,year,month)
+        os.symlink(os.path.join(srcdir, filename), os.path.join(linkdir, filename))
+
+for year in ['0101','0102','0103','0104','0105','0106','0107','0108','0109','0110','0111','0112','0113','0114','0115','0116','0117','0118','0119','0120']:
+    for month in ['01','02','03','04','05','06','07','08','09','10','11','12']:
+        
+        srcdir = '/glade/p/cesm0005/archive/b.e20.B1850.f09_g17.pi_control.all.201/ocn/hist'
+        linkdir = '/glade/scratch/aliceb/b.e20.B1850.f09_g17.pi_control.all.201/ocn/hist'
+
+        if not os.path.exists(linkdir):
+            os.makedirs(linkdir)
+
+        casename = 'b.e20.B1850.f09_g17.pi_control.all.201.pop.h'
         filename = '{0}.{1}-{2}.nc'.format(casename,year,month)
         os.symlink(os.path.join(srcdir, filename), os.path.join(linkdir, filename))
