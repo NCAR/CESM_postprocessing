c5f8b631a7e706fad252d01b7ad8f36bbee73da6
Jim Edwards <jedwards@ucar.edu>
2020-09-18 11:42:09 -0600

    change urlopen import method

--- diagnostics/diagnostics/ice/create_ice_html.py
+++ diagnostics/diagnostics/ice/create_ice_html.py
@@ -1,4 +1,5 @@
-import os, urllib, glob, datetime
+import os, glob, datetime
+from urllib.request import urlopen
 
 def create_plotset_html(html_file, new_fn, env):
 
@@ -6,7 +7,7 @@ def create_plotset_html(html_file, new_fn, env):
     img_t = env['PLOT_FORMAT']
 
     # If new htm file exists, remove it
-    if os.path.isfile(new_fn): 
+    if os.path.isfile(new_fn):
         os.remove(new_fn)
     new_html = open(new_fn,'w')
     print('DEBUG: create_ice_html new_html = {0}'.format(new_html))
@@ -14,37 +15,36 @@ def create_plotset_html(html_file, new_fn, env):
     web_path = os.path.dirname(new_fn)+'/'
     print('DEBUG: create_ice_html web_path = {0}'.format(web_path))
 
-    # Open html template 
+    # Open html template
     print('DEBUG: create_ice_html html_file = {0}'.format(html_file))
-    data = urllib.urlopen(html_file)
+    data = urlopen(html_file)
 
-    # Loop through lines in partial html file.  If a href (image link) check to see 
+    # Loop through lines in partial html file.  If a href (image link) check to see
     # if image exists and handle correctly
     for line in data.readlines():
         if 'CASENAME' in line:
             line = line.replace('CASENAME',env['CASE_TO_CONT'])
         elif 'HREF' in line:
-            # Split the element up    
+            # Split the element up
             elements = line.split('>')
             i = 0
             for elem in elements:
                 if 'HREF' in elem:
                     link_tag_parts = elements[i].split('HREF=')
                     image_name = link_tag_parts[1].replace('\"','')
-                i = i+1 
+                i = i+1
             if 'png' in image_name:
                 image_name = image_name.replace('png',img_t)
                 if not os.path.isfile(web_path+'/'+image_name):
                     print("NOT FOUND: "+web_path+'/'+image_name)
                     for ln in ('JFM','AMJ','JAS','OND','ANN','FM','ON','plot'):
-                        if ln in line: 
-                            line = line.replace(ln,'----') 
+                        if ln in line:
+                            line = line.replace(ln,'----')
                 else:
                     line = line.replace('png',img_t)
-             
+
         new_html.write(line)
     new_html.write('<b> Plots Created </b><br>')
     date = datetime.datetime.now().strftime("%a %b %d %H:%M:%S %Y")
     new_html.write(date)
-    new_html.close() 
-
+    new_html.close()
